c6518d465d4b6487ce03e5fd8800f70d82f0898b
Robert Garcia <robgarciaxyz@gmail.com>
2020-05-15 19:31:59 -0400

    setting up brewskis search route

---
+++ routes/brewskis.js
@@ -0,0 +1,26 @@
+const { Router } = require('express')
+const router = Router()
+const axios = require('axios')
+
+router.get('/api/brewskis', async(request, response) => {
+    const {search} = request.query
+    const url = `https://api.openbrewerydb.org/breweries/search?query=${search}`
+    const {data} = await axios.get(url)
+    const myArray = []
+    data.map(brewery => {
+        const myObject = {
+            name: brewery.name,
+            type: brewery.brewery_type,
+            street: brewery.street,
+            city: brewery.city,
+            state: brewery.state,
+            postal: brewery.postal_code,
+            phone: brewery.phone,
+            website: brewery.website_url
+        }
+        myArray.push(myObject)
+    })
+    response.json(myArray)
+})
+
+module.exports = router
