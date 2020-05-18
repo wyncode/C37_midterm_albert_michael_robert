8e6c674647a6427254cdda93676f0fd42010b278
Robert Garcia <robgarciaxyz@gmail.com>
2020-05-18 15:26:45 -0400

    set up react router for individual breweries page

---
+++ client/src/components/IdBrewery.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import Container from 'react-bootstrap/Container'
+
+const IdBrewery = () => {
+  // const { id } = props.match.params
+//   const id = props.match.brewery;
+    const [apiData, setApiData] = useState({});
+
+    const { id } = useParams();
+
+  const fetchIdBrewery = () => {
+    axios.get(`/api/brewskis/${id}`)
+    .then((res) => {
+      console.log('id', id);
+    setApiData(res.data);
+      console.log(res);
+      // set state here
+    }).catch ((e) =>  {
+        console.log(e)
+    });
+  };
+
+  const zip = apiData.postal_code
+
+  useEffect(fetchIdBrewery, []);
+  return (
+    <Container>
+        <h1>{apiData.name}</h1>
+        <h2>{apiData.street}</h2>
+        <h2>{apiData.city}, {apiData.state}  {zip}</h2>
+        
+    </Container>
+  )
+};
+
+export default IdBrewery;
